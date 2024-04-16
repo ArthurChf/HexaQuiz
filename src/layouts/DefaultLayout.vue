98d029d5d44d78144998688d0027be6394a9ffd8
Arthur <chaff.arthur@gmail.com>
2024-04-16 13:35:50 +0200

    add loading screen (work in progress)

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="bg-background flex flex-col min-h-screen">
+        <div class="loading-screen">
+            <img src="@/assets/images/logo.png" alt="Logo" />
+            <div class="progress-bar__container">
+                <div class="progress-bar__content"></div>
+            </div>
+        </div>
         <RouterView #default="{ Component }">
             <Transition :name="TransitionEnum.FADE" mode="out-in">
                 <KeepAlive>
