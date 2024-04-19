ac91ae262f781730d3eb08433d76369416ba61d1
Arthur <chaff.arthur@gmail.com>
2024-04-19 12:12:57 +0200

    Image component

---
+++ src/components/AppImage.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src="getImageUrl(name)" :style="{ width: `${size}px`, height: 'max-content' }" />
+</template>
+
+<script setup lang="ts">
+import type { ImageEnum } from '@/enums/ImageEnum';
+
+defineProps<{
+    name: ImageEnum;
+    size: string;
+}>();
+
+const getImageUrl = (path: string) => {
+    return new URL(`../assets/images/${path}`, import.meta.url).href;
+};
+</script>
