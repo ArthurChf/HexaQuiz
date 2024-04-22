f5789664f8fb28fb8872315528c62192b2a5afac
Arthur <chaff.arthur@gmail.com>
2024-04-22 20:29:47 +0200

    Progress bar component

---
+++ src/components/AppProgressBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="progress-bar__container">
+        <div class="progress-bar__content" :style="{ width: `${width}%` }"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    width: number;
+}>();
+</script>
